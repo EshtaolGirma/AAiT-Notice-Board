e03d524bef4587f65bc5a7627bd50d215a38480a
Blen92 <se.blen.ali92@gmail.com>
2021-10-12 08:06:51 +0300

    databae conected

---
+++ Back-End/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+CourseMatRouter = require('./routes/CourseMatRoute');
+CourseRouter = require('./routes/CourseRoute');
+DepartRouter = require('./routes/DepartRoute')
+DiscussionRouter = require('./routes/DiscussionRoute')
+NewsFeedRouter = require('./routes/NewsFeedRoute')
+UserRouter = require('./routes/UserRoute');
+
+app.use("/api/Course", CourseRouter);
+// app.use("/api/CourseMaterial", CourseMatRouter);
+// app.use("/api/Department", DepartRouter);
+// app.use("/api/Discussion", DiscussionRouter);
+// app.use("/api/NewsFeed", NewsFeedRouter);
+// app.use("/api/User", UserRouter);
+
+
+const DB_URI = 'mongodb://localhost:27017/manjuDB'
+
+mongoose.connect(DB_URI).then(() => {
+    app.listen(3000, () => console.log('Server running...'));
+})
+    .catch((err) => console.log('Error occurred while connecting', err));
+
+
+module.exports.app = app
+
