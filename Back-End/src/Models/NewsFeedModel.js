30d97805d0c4245cbf015e8a11ac9c1ff99f62d0
shewit <se.shewit.ghiwot@gmail.com>
2021-10-13 01:37:03 +0300

    NewsFeed CRUD

---
+++ Back-End/src/Models/NewsFeedModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const NewsFeedSchema = mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        title:{
+            type: String,
+            required: true,
+
+        },
+        description:{
+            type: String,
+            required: true,
+        },
+        deptId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Deparment',
+            required: true,
+        },
+        postDate:{
+            type: Date,
+            required: true
+        }
+
+    }
+)
+
+module.exports = mongoose.model('NewsFeed', NewsFeedSchema);
---
+++ Back-End/src/controllers/NewsFeedController.js
@@ -0,0 +1,127 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const NewsFeed = require('../Models/NewsFeedModel');
+const Department = require("../Models/DepartModel");
+
+module.exports ={
+    //GET ALL    
+        getAll:(req, res) =>{
+            NewsFeed.find().populate("deptId").exec()
+            .then(result =>{
+                console.log(result);
+                res.status(200).json({
+                    message: 'Fetched succefully',
+                    data: result})
+            }).catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    message:'Faced an error while fetching',
+                    error: err
+                })
+            })
+        },
+    //GET BY ID
+        getOne: (req, res)=>{
+            const id = req.params.newsId;
+            NewsFeed.find({_id:id}).populate("deptId").exec()
+            .then(result =>{
+                if(result){
+                    console.log(result)
+                    res.status(200).json({
+                        message: 'Fetched successfully',
+                        data: result})
+                }else{
+                    res.status(404).json({
+                        message:'No data with the entry provided',
+                        
+                    }) 
+                }
+                
+            }).catch(err=>{
+                console.log(err);
+                res.status(500).json({
+                    message:'Faced an error while fetching',
+                    error: err
+                })
+            }) 
+            
+        },
+    //DELETE
+        delete: (req, res)=>{
+            const id = req.params.instId;
+            NewsFeed.remove({_id: id}).exec()
+            .then(result => {
+                if(result){
+                    console.log(result);
+                    res.status(200).json({
+                        message:'Data has been successfully deleted',
+                        Deletedata: result
+                    })
+    
+                }else{
+                    res.status(404).json({
+                        message:'No data to be deleted with the entry provided',
+                        
+                    }) 
+                }
+                
+            }).catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    message:'Faced an error while deleting',
+                    error: err
+                })
+            })
+           
+        },
+    //POST
+        post: (req, res)=>{
+            const newsFeed = new NewsFeed({
+                _id: new mongoose.Types.ObjectId,
+                title: req.body.title,
+                description: req.body.description,
+                deptId: req.body.deptId,
+                postDate: req.body.postDate
+               
+    
+            })
+            newsFeed.save()
+            .then(result => {
+                // Department.updateOne({_id: req.body.deptId}, {$push:{newsFeed: newsFeed._id }}).exec();
+                res.status(200).json({
+                    message:'Posted successfully',
+                    PostedData: result
+                });
+            }).catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    message:'Faced an error while posting',
+                    err: err
+                })
+    
+            })
+    
+        },
+    
+     //UPDATE   
+        put: (req, res)=>{
+            
+            NewsFeed.findByIdAndUpdate({ _id: req.params.newsId},
+                { $set: req.body})
+            .exec()
+            .then(result =>{
+                console.log(result);
+                res.status(200).json({
+                    message: 'Data updated successfully',
+                    UpdatedData: result
+                })
+            }).catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    message:'Faced an error while updating',
+                    error: err
+                })
+            })
+        }
+    
+    };
