abf4f17314fd7613ad88da923bcffdb9a3b2edae
Eshtaol Girma Bayessa <it.eshtaol.girma@gmail.com>
2021-11-01 21:36:56 +0300

    auth modified a bit

--- Back-End/src/middleware/auth.js
+++ Back-End/src/middleware/auth.js
@@ -2,8 +2,11 @@ const jwt = require("jsonwebtoken");
 const TOKEN_KEY = "thisisthesecretkey";
 
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+  // const token =
+  //   req.body.token || req.query.token || req.headers["x-access-token"];
+  const authHeader = req.headers["authorization"];
+  const token1 = authHeader.split(" ")["1"];
+  const token = req.body.token || req.query.token || token1;
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
