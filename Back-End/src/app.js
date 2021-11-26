467094d147be45a46230db36d67436c756670a82
Eshtaol Girma Bayessa <it.eshtaol.girma@gmail.com>
2021-11-26 19:27:31 +0300

    refined

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -22,14 +22,14 @@ CourseRouter = require("./routes/CourseRoute");
 DepartRouter = require("./routes/DepartRoute");
 DiscussionRouter = require("./routes/DiscussionRoute");
 NewsFeedRouter = require("./routes/NewsFeedRoute");
-UserRouter = require("./routes/UserRoute");
+// user route import
 
 app.use("/api/Course", CourseRouter);
 app.use("/api/CourseMaterial", CourseMatRouter);
 app.use("/api/Department", DepartRouter);
 app.use("/api/Discussion", DiscussionRouter);
 app.use("/api/NewsFeed", NewsFeedRouter);
-app.use("/api/User", UserRouter);
+// user router connection
 
 const DB_URI =
   "mongodb+srv://aait-notice-board:aait-notice-board@aait-notice-board.qq1go.mongodb.net/manjuDB?retryWrites=true&w=majority";
